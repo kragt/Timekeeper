35c8cd2e1193997ffdca180996f5130bf91c6fde
Tomas Krag Hansen <6706947+kragt@users.noreply.github.com>
2021-03-25 14:36:39 +0100

    New webapp

---
+++ src/Timekeeper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Timekeeper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
